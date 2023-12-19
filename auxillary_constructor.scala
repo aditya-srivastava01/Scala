6c9a48745bd2cf1a79f70637c33bf27277d2d4d9
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2023-12-19 12:50:49 +0530

    new push

---
+++ auxillary_constructor.scala
@@ -0,0 +1,18 @@
+class Rational(n: Int ,d : Int){
+    val num : Int = n
+    val denom : Int = d
+
+    def this(n : Int) = this(n,1)
+
+    def display()={
+        println(s"$n/$d")
+    }
+}
+object Rational_Constructor{
+    def main(args : Array[String])={
+        val x = new Rational(1,2)
+        x.display()
+        val y = new Rational(6)
+        y.display()
+    }
+}
